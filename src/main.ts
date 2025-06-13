0cb5f8450fead57a4287e7017cfa4c836e5e0342
Yeferson Huarachi <roberthuarachi27@gmail.com>
2025-06-13 10:58:24 -0400

    config main.ts V2

--- src/main.ts
+++ src/main.ts
@@ -17,10 +17,14 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'https://webcolaborativaux-cgfaamc3gggwegg6.eastus-01.azurewebsites.net/',
-      'http://18.204.21.109:3000'
+      'http://18.204.21.109:3000',
+      'http://100.24.58.238:3001'
     ], // URL de tu frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Añadir OPTIONS para preflight
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // Especificar los headers permitidos
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   });
   await app.listen(port, '0.0.0.0', ()=> {
     Logger.log(`Server in port ${port}`)
